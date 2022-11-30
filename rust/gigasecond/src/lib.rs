4f6bd3cd561d333545e307fefc23020a058a2115
Daniel Sank email <sank.daniel@gmail.com>
2022-11-29 21:15:03 -0800

    Improve gigasecond solution.

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,7 +1,7 @@
 use time::PrimitiveDateTime as DateTime;
-use time::Duration as Duration;
+use time::ext::NumericalDuration;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    start.checked_add(Duration::seconds(1000000000)).unwrap()
+    start.checked_add((1000000000_i64).seconds()).unwrap()
 }
